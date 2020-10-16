69554966c951c710316bd32f3b859fe67ad31d14
startec-official <startec.innovations@gmail.com>
2020-10-16 16:50:53 +0800

    added log

--- connections/serial-connection.js
+++ connections/serial-connection.js
@@ -2,7 +2,7 @@ const SerialPort = require('serialport')
 const dotenv = require('dotenv')
 
 dotenv.config()
-
+console.log( `${process.env.SERIAL_PORT}` );
 const port = new SerialPort( process.env.SERIAL_PORT , function (err) {
   if (err) {
     return console.log('Cannnot open device, running as server only...') // TODO: error handling on startup here
